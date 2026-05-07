6177386e476415aa2fdc23898c9388dfab30d6ca
MiltonRafael <miltonrafael166@gmail.com>
2026-05-07 18:10:46 -0300

    Testando o database seeding e feito ambiente de teste

---
+++ backend/src/main/java/com/fasipe/backend/repositories/RegisterRepository.java
@@ -0,0 +1,5 @@
+package com.fasipe.backend.repositories;
+
+public interface RegisterRepository {
+
+}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_register (name, course, date) VALUES ('Milton', 'Analise e desenvolvimento de sistemas', now());
