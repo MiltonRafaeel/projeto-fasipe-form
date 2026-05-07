86aa87b61da8b1f6be91e1c9cf653c6060da2e6a
MiltonRafael <miltonrafael166@gmail.com>
2026-05-07 17:20:42 -0300

    Projeto criado e classe registro feita

---
+++ backend/src/main/java/com/fasipe/backend/entities/Register.java
@@ -0,0 +1,82 @@
+package com.fasipe.backend.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_register")
+public class Register {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String course;
+	private Instant date;
+	
+	public Register() {
+		
+	}
+
+	public Register(Long id, String name, String course, Instant date) {
+		this.id = id;
+		this.name = name;
+		this.course = course;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Register other = (Register) obj;
+		return Objects.equals(id, other.id);
+	}
+}
